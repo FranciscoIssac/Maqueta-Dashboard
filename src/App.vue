8e43d75b41ae3b33d7569e797feab31e9f715fbd
cristopher83 <cristopher34234@gmail.com>
2024-11-16 21:14:31 -0600

    Simple card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
 import Counter from "./components/Counter.vue";
 import Lista from "./components/ListReactive.vue";
+import Card from "./components/Card.vue";
 </script>
 
 <!-- <template>
   <Counter></Counter>
 </template> -->
 
-<template>
+<!-- <template>
   <Lista></Lista>
+</template> -->
+
+<template>
+  <Card>
+    <template #title>Welcome</template>
+    <template #description>This is a simple card for example.</template>
+  </Card>
 </template>
 
 <style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h1>
+    <slot name="title"></slot>
+  </h1>
+  <p>
+    <slot name="description"></slot>
+  </p>
+</template>
+
+<style></style>

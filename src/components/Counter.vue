3c1f778dbdda975d74c08d0b707fef63a2f24594
cristopher83 <cristopher34234@gmail.com>
2024-11-16 20:31:28 -0600

    Ref and Component, Counter example

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+const contador = ref(0);
+const increment = () => {
+  contador.value++;
+};
+const decrement = () => {
+  contador.value--;
+};
+</script>
+
+<template>
+  <button @click="decrement">Decrement</button>
+  <h1>{{ contador }}</h1>
+  <button @click="increment">Increment</button>
+</template>
+
+<style scoped>
+button {
+  background-color: gray;
+}
+</style>

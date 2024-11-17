d0d89133e87898dd49736f4c60cf98362f27033e
cristopher83 <cristopher34234@gmail.com>
2024-11-16 21:05:44 -0600

    Reactive title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Counter from "./components/Counter.vue";
+import Lista from "./components/ListReactive.vue";
 </script>
 
-<template>
+<!-- <template>
   <Counter></Counter>
+</template> -->
+
+<template>
+  <Lista></Lista>
 </template>
 
 <style scoped></style>
---
+++ src/components/ListReactive.vue
@@ -0,0 +1,29 @@
+<!-- this component allows you to write something and show it with enter, the input has a variable and a directive -->
+<script setup>
+import { ref } from "vue";
+const title = ref("");
+const newTitle = ref("");
+
+const changeTitle = () => {
+  title.value = newTitle.value;
+  newTitle.value = "";
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <div class="input-field">
+    <label for="">Write in the field to change the title</label>
+    <input type="text" v-model="newTitle" @keyup.enter="changeTitle" />
+  </div>
+</template>
+
+<style>
+.input-field {
+  display: flex;
+  flex-direction: column;
+  & input {
+    width: 300px;
+  }
+}
+</style>

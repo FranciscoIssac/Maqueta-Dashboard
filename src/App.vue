944179f2100b119f9bf469f0bc5d532e55ea1313
Issac <a276679@alumnos.uaslp.mx>
2024-11-20 11:38:30 -0600

    Botones terminados

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import DownloadButton from "./components/DownloadButton.vue";
 import EvaluateButton from "./components/EvaluateButton.vue";
 import AcceptButton from "./components/AcceptButton.vue";
 import CancelButton from "./components/CancelButton.vue";
+import CustomButton from "./components/CustomButton.vue";
 import UserButton from "./components/UserButton.vue";
 import MailIcon from "./components/icons/Mail.vue";
 import BellIcon from "./components/icons/Bell.vue";
@@ -39,9 +40,45 @@ import PersonIcon from "./components/icons/Person.vue";
   </Tablero>
 </template> -->
 
-<template>
+<!-- <template>
   <UserButton>
+    <template #user>
+      Administrador
+    </template>
   </UserButton>
+</template> -->
+
+<template>
+  <div>
+    <CustomButton
+    text=""
+      backgroundColor="#004A98"
+      width="100"
+      height="40"/>
+
+    <!-- <CustomButton 
+      text="Aceptar" 
+      backgroundColor="#004A98" 
+      width="206" 
+      height="50"
+    /> -->
+
+    <!-- <CustomButton 
+      text="Cancelar" 
+      backgroundColor="#00B2E3" 
+      width="206" 
+      height="50"
+    /> -->
+
+    <!-- <CustomButton 
+      text="Descargar" 
+      backgroundColor="#004A98" 
+      width="200" 
+      height="50"
+    /> -->
+
+  </div>
 </template>
+
 
 <style scoped></style>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,60 @@
+<template>
+    <button :class="buttonClass"
+        :style="{ backgroundColor: backgroundColor, width: width + 'px', height: height + 'px', color: color }">
+        <span>{{ text }}</span>
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+    text: {
+        type: String,
+        default: "Button",
+    },
+    backgroundColor: {
+        type: String,
+        default: "#004A98",
+    },
+    color: {
+        type: String,
+        default: "white",
+    },
+    width: {
+        type: Number,
+        default: 200,
+    },
+    height: {
+        type: Number,
+        default: 50,
+    },
+    buttonClass: {
+        type: String,
+        default: "",
+    },
+});
+</script>
+
+<style scoped>
+button {
+    font-family: "Open Sans";
+    font-size: 20px;
+    font-weight: 700;
+    line-height: normal;
+    border: none;
+    border-radius: 24px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 0 12px;
+    transition: background-color 0.3s;
+}
+
+button:hover {
+    filter: brightness(0.9);
+}
+</style>
